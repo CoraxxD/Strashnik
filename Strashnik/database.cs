f0d6c9f82507ebde5463de36ee3f64cc8dfdf180
stud <stud@almetpt.local>
2023-04-13 09:16:40 +0300

    pushim pustoi proekt dlya commita

---
+++ Strashnik/database.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Strashnik
+{
+    class database
+    {
+        SqlConnection conn = new SqlConnection("Data Source = it224-14;Initial Catalog=Strashnik;Integrated Security=True");
+
+        public void openConnection()
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return conn;
+        }
+    }
+}
